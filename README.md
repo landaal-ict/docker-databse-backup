56a58bd57ef7bb6ecb06a0eb782546fd4c5a705a
Landaal-ict <wesley@familielandaal.nl>
2022-01-07 17:12:26 +0100

    Backup script voor docker database backups

---
+++ README.md
@@ -0,0 +1,23 @@
+# Docker Database Backup
+
+Simpel backup script voor backups van je database containers
+
+
+## Ondersteuning?
+
+- [x] Mysql
+- [x] Mariadb
+- [x] Bitwardenrs database (NIET BITWARDEN ZELF)
+
+## Hoe te gebruiken.
+
+- Clone de repo
+- Verander het aantal dagen en de backup locatie in backup.sh 
+- draai het script handmatig of automatische via cron.
+
+## Eigen risico
+
+Ik gebruik mijn scripts voor eigen doelen.
+Dat ze bij mij werken wil niet zeggen dat het bij jou ook werkt.
+
+Gebruik nooit zo maar gedownloade scripts, kijk eerst in de code wat ze doen! 
---
+++ backup.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# DB Container Backup Script
+# 
+#
+# Dit backup script kan gebruikt worden voor het backuppen van database containers.
+# Ondersteunt mariadb, mysql and bitwardenrs
+# 
+#-------------------------------------------------------------------
+# Vul hier onder het aantal dagen dat je de backups wilt bewaren.
+# Geef ook je backup dir aan, als de locatie niet bestaad maakt dit script hem aan.
+DAGEN=2
+BACKUPDIR=/pool/database_backups
+
+
+# backup alle mysql,mariadb containers
+
+CONTAINER=$(docker ps --format '{{.Names}}:{{.Image}}' | grep 'mysql\|mariadb' | cut -d":" -f1)
+
+echo $CONTAINER
+
+if [ ! -d $BACKUPDIR ]; then
+    mkdir -p $BACKUPDIR
+fi
+
+for i in $CONTAINER; do
+    MYSQL_DATABASE=$(docker exec $i env | grep MYSQL_DATABASE |cut -d"=" -f2)
+    MYSQL_PWD=$(docker exec $i env | grep MYSQL_ROOT_PASSWORD |cut -d"=" -f2)
+
+    docker exec -e MYSQL_DATABASE=$MYSQL_DATABASE -e MYSQL_PWD=$MYSQL_PWD \
+        $i /usr/bin/mysqldump -u root $MYSQL_DATABASE \
+        | gzip > $BACKUPDIR/$i-$MYSQL_DATABASE-$(date +"%d%m%Y%H%M").sql.gz
+
+    OLD_BACKUPS=$(ls -1 $BACKUPDIR/$i*.gz |wc -l)
+    if [ $OLD_BACKUPS -gt $DAGEN ]; then
+        find $BACKUPDIR -name "$i*.gz" -daystart -mtime +$DAYS -delete
+    fi
+done
+
+
+# bitwardenrs backup
+# ALLEEN voor bitwardenrs NIET voor bitwarden zelf!
+
+BITWARDEN_CONTAINERS=$(docker ps --format '{{.Names}}:{{.Image}}' | grep 'bitwardenrs' | cut -d":" -f1)
+
+for i in $BITWARDEN_CONTAINERS; do
+    docker exec  $i /usr/bin/sqlite3 data/db.sqlite3 .dump \
+        | gzip > $BACKUPDIR/$i-$(date +"%Y%m%d%H%M").sql.gz
+
+    OLD_BITWARDEN_BACKUPS=$(ls -1 $BACKUPDIR/$i*.gz |wc -l)
+    if [ $OLD_BITWARDEN_BACKUPS -gt $DAYS ]; then
+        find $BACKUPDIR -name "$i*.gz" -daystart -mtime +$DAYS -delete
+    fi
+done
+
+echo "$TIMESTAMP Backup voor Databases voltooid"
